593f4b64824ad2994046fd1c8744327578915a36
서라면 <nayeon@new16.local>
2020-09-03 23:50:36 +0900

    Add chart unut

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,134 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { select, Selection,  } from 'd3-selection';
+import React, { useEffect, useRef, useState } from "react";
+import { select, Selection } from "d3-selection";
+import { scaleLinear, scaleBand } from "d3-scale";
+import { max } from "d3-array";
+import randomstring from "randomstring";
 
-const data = [
-	{
-		unit: 150,
-		color: 'yellow'
-	},
-	{
-		unit: 100,
-		color: 'orange'
-	},
-	{
-		unit: 50,
-		color: 'red'
-	},
-	{
-		unit: 70,
-		color: 'teal'
-	},
-	{
-		unit: 120,
-		color: 'pink'
-	}
+let initialData = [
+  {
+    name: "foo",
+    unit: 32,
+  },
+  {
+    name: "bar",
+    unit: 67,
+  },
+  {
+    name: "bbb",
+    unit: 81,
+  },
+  {
+    name: "ccc",
+    unit: 38,
+  },
+  {
+    name: "ddd",
+    unit: 28,
+  },
+  {
+    name: "hordor",
+    unit: 59,
+  },
 ];
 
+const dimensions = {
+  width: 900,
+  height: 600,
+};
+
 const App: React.FC = () => {
-	const ref = useRef(null);
-	const [selection, setSelection] = useState<null | Selection<
-		null,
-		unknown,
-		null,
-		undefined
-	>>(null);
+  const ref = useRef<SVGSVGElement | null>(null);
+  const [selection, setSelection] = useState<null | Selection<
+    SVGSVGElement | null,
+    unknown,
+    null,
+    undefined
+  >>(null);
 
-	useEffect(() => {
-		if (!selection) {
-			setSelection(select(ref.current));
-		} else {
-			const rects = selection
-				.selectAll('rect')
-				.data(data)
-				.attr('width', 100)
-				.attr('height', d => d.unit)
-				.attr('fill', d => d.color)
-				.attr('x', (_, i) => i * 100);
+  const [data, setData] = useState(initialData);
 
-			rects
-				.enter()
-				.append('rect')
-				.attr('width', 100)
-				.attr('height', d => d.unit)
-				.attr('fill', d => d.color)
-				.attr('x', (_, i) => i * 100);
-			console.log(rects);
-		}
-	}, [selection]);
+  let y = scaleLinear()
+    .domain([0, max(data, (d) => d.unit)!])
+    .range([dimensions.height, 0]);
 
+  let x = scaleBand()
+    .domain(data.map((d) => d.name))
+    .range([0, dimensions.width])
+    .paddingInner(0.05);
 
+  useEffect(() => {
+    if (!selection) {
+      setSelection(select(ref.current));
+    } else {
+      selection
+        .selectAll("rect")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("width", x.bandwidth)
+        .attr("height", (d) => dimensions.height - y(d.unit))
+        .attr("x", (d) => x(d.name)!)
+        .attr("y", (d) => y(d.unit))
+        .attr("fill", "orange");
+    }
+  }, [selection]);
 
-	return (
-		<div>
-			{/* 기본 너비는 300임. 500으로 확장필요 */}
-			<svg ref={ref} width={500}>
-				<rect />
-			</svg>
-		</div>
-	);
+  useEffect(() => {
+    if (selection) {
+      y = scaleLinear()
+        .domain([0, max(data, (d) => d.unit)!])
+        .range([dimensions.height, 0]);
+
+      x = scaleBand()
+        .domain(data.map((d) => d.name))
+        .range([0, dimensions.width])
+        .paddingInner(0.05);
+
+      const rects = selection.selectAll("rect").data(data);
+      rects.exit().remove();
+
+      rects
+        .attr("width", x.bandwidth)
+        .attr("height", (d) => dimensions.height - y(d.unit))
+        .attr("x", (d) => x(d.name)!)
+        .attr("y", (d) => y(d.unit))
+        .attr("fill", "orange");
+
+      rects
+        .enter()
+        .append("rect")
+        .attr("width", x.bandwidth)
+        .attr("height", (d) => dimensions.height - y(d.unit))
+        .attr("x", (d) => x(d.name)!)
+        .attr("y", (d) => y(d.unit))
+        .attr("fill", "orange");
+    }
+  }, [data]);
+
+  const addRandom = () => {
+    const dataToBeAdded = {
+      name: randomstring.generate(10),
+      unit: Math.floor(Math.random() * 80 + 20),
+    };
+    setData([...data, dataToBeAdded]);
+  };
+
+  const removeLast = () => {
+    if (data.length === 0) {
+      return;
+    }
+    const slicedData = data.slice(0, data.length - 1);
+    setData(slicedData);
+  };
+
+  return (
+    <div>
+      {/* 기본 너비는 300임. 500으로 확장필요 */}
+      <svg ref={ref} width={dimensions.width} height={dimensions.height} />
+      <button onClick={addRandom}>addRandom</button>
+      <button onClick={removeLast}>removeLast</button>
+    </div>
+  );
 };
 
 export default App;
