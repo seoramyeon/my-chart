ddd8800a4564711572f13a7bd8f4342d10be23fd
서라면 <nayeon@new16.local>
2020-09-10 23:48:17 +0900

    Add chart axis

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
 import React, { useRef, useEffect, useState } from 'react';
 import './App.css';
-import { select , line, curveCardinal} from 'd3';
+import {
+  select,
+  line,
+  curveCardinal,
+  axisBottom,
+  axisRight,
+  scaleLinear,
+} from 'd3';
+// import { axisRight } from 'd3-axis';
 
 //useRef: DOM 을 직접 선택해야 하는 상황이 발생 할 때도 있습니다.
 //ex)특정 엘리먼트의 크기를 가져와야 한다던지,
-//스크롤바 위치를 가져오거나 설정해야된다던지, 또는 포커스를 설정해줘야된다던지 등
 
 const App = () => {
-  const [data, setData] = useState([25, 30, 45, 60, 20, 80]);
+  const [data, setData] = useState([25, 30, 45, 60, 20, 65, 80]);
   const svgRef = useRef();
 
   useEffect(() => {
     const svg = select(svgRef.current);
+    const xScale = scaleLinear()
+      .domain([0, data.length - 1])
+      .range([0, 300]);
+
+    const yScale = scaleLinear().domain([0, 150]).range([150, 0]);
+    const xAxis = axisBottom(xScale)
+      .ticks(data.length)
+      .tickFormat((index) => index + 1);
+    svg.select('.x-axis').style('transform', 'translateY(150px)').call(xAxis);
+
+    const yAxis = axisRight(yScale);
+    svg.select('.y-axis').style('transform', 'translateX(300px)').call(yAxis);
+
     const myLine = line()
-      .x((value, index) => index * 50)
-      .y(value => 150 - value)
-      .curve(curveCardinal)
+      .x((value, index) => xScale(index))
+      .y(yScale)
+      .curve(curveCardinal);
+    //원 그래프
     // svg
     //   .selectAll('circle')
     //   .data(data)
@@ -26,17 +47,24 @@ const App = () => {
     //   .attr('stroke', 'pink');
 
     svg
-      .selectAll('path')
+      .selectAll('.line')
       .data([data])
       .join('path')
-      .attr('d', (value) => myLine(value))
+      .attr('class', 'line')
+      .attr('d', myLine)
       .attr('fill', 'none')
       .attr('stroke', 'pink');
   }, [data]);
 
   return (
     <React.Fragment>
-      <svg ref={svgRef} width={600} height={400}></svg>
+      <svg ref={svgRef}>
+        <g className='x-axis' />
+        <g className='y-axis' />
+      </svg>
+      <br />
+      <br />
+      <br />
       <br />
       <button onClick={() => setData(data.map((value) => value + 5))}>
         Update data
